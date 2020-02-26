d2b0d5caf0a5f40647ab042eecc7373d8b29ee3d
Ahmad Sharif <TheAhmadSharif@gmail.com>
2020-02-26 10:38:26 +0600

    Update Edit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  editDiv:boolean  = false;
+  editMember: any;
+  id: any;
+  teams = ["Mehrab Miah", "Sunom Singha","Ahmad Sharif", "Mustafizur Rahman Khan"];
+  addItem = function (newmember: any) {
+    this.teams.push(newmember.value);
+    newmember.value = '';
+  }
+  removeItem = function (id:any) {
+    this.teams.splice(id, 1);
+  }
+  editItem = function (id:any) {
+    this.editDiv =! this.editDiv;
+    this.editMember = this.teams[id];
+    this.id = id;
+  }
+
+  updateItem = function (id:any, editMember:any) {
+    this.teams[id] = editMember;
+    this.editDiv =! this.editDiv;
+  }
+    
+}
